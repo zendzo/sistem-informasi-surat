30cdb8c630b34fee9f9847dae438915929ae033a
zendzo <zendzzo@gmail.com>
2018-09-01 22:23:44 +0700

    add home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // redirect to admin page is user is ad administrator
+        if (Auth::user()->role_id === 1) {
+            return redirect('/admin');
+        }
+
         return view('home');
     }
 }
