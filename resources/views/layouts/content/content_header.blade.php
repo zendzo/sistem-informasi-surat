f58b5e9e474bcf6ec7aff5eb8c01a3dd4d3acc0f
zendzo <zendzzo@gmail.com>
2018-09-01 22:30:45 +0700

    seprate view to folders

---
+++ resources/views/layouts/content/content_header.blade.php
@@ -0,0 +1,11 @@
+<section class="content-header">
+<h1>
+    {{ $page_title or config('app.name') }}
+    <small>{{ $page_desc or config('app.desc') }}</small>
+</h1>
+<ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="#">Examples</a></li>
+    <li class="active">Blank page</li>
+</ol>
+</section>
