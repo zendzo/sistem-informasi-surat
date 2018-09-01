fc187ea2f93ec9023e8e938d3f9b7e05d816aaf4
zendzo <zendzzo@gmail.com>
2018-09-01 22:23:03 +0700

    add administrator gourp section

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+})->middleware('guest');
+
+// Administrator Sections
+Route::group(['prefix'=>'admin','as'=>'admin.','middleware'=>'administrator'], function(){
+
+	Route::get('/',[
+		'as'	=>	'dashboard',
+		'uses'	=>	'AdministratorController@index'
+	]);
 });
 
 Auth::routes();
