278f11bf25791c3860daf1e9615f759d3088e175
zendzo <zendzzo@gmail.com>
2018-09-01 22:40:16 +0700

    fit typo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(GendersTableSeeder::class);
-        $this->call(AdminTableSeeder::class);
+        $this->call(AdminSeeder::class);
     }
 }
