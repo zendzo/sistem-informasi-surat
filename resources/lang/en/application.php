a98c7db8d3aab452d7b697904692d19747899fc1
zendzo <zendzzo@gmail.com>
2018-09-02 14:47:08 +0700

    add app language

---
+++ resources/lang/en/application.php
@@ -0,0 +1,14 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Applications Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'menu setting' => 'Menu Settings',
+    'menu desc'   => 'Organize Menus',
+    'add new record'     => 'Add New Record',
+
+];
